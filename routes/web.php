6bafc4071181b83bfec6609e4a5b1eb2a7d98042
Undefined <lirikrexhepiii@gmail.com>
2023-02-07 15:10:27 +0100

    webhook for hosting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\RegisterController;
+
+//use App\Http\Controllers\WebhookController;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Artisan;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,6 +16,30 @@ use App\Http\Controllers\API\RegisterController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::post('/webhook', 'WebhookController@handle');
+//Route::post('/webhook', [WebhookController::class, 'handle']);
+
+Route::post('/webhooks/github', function() {
+
+    // Code to handle webhook request and pull changes from Github
+    //$output = shell_exec('cd /var/www/Ritwell-App && git pull');
+    //Log::info("Git pull output: \n" . $output);
+      exec('cd /var/www/Ritwell-App && git pull ', $output, $return_var);
+
+ 
+
+if ($return_var !== 0) {
+    Log::error("Git pull failed with return code: " . $return_var);
+} else {
+    Log::info("Git pull output: \n" . implode("\n", $output));
+    Artisan::call('optimize');
+}
+});
 
 Route::get('/', function () {
     return view('welcome');
