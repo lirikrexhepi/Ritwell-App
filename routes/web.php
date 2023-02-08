1a2ad44a3b06d9859a28c4ff1a8e8d498fb2d8a1
Undefined <lirikrexhepiii@gmail.com>
2023-02-08 15:10:55 +0100

    testFile

--- routes/web.php
+++ routes/web.php
@@ -24,21 +24,21 @@ Route::get('/', function () {
 //Route::post('/webhook', 'WebhookController@handle');
 //Route::post('/webhook', [WebhookController::class, 'handle']);
 
-Route::post('/webhooks/github', function() {
+Route::post('/webhooks/github', function () {
 
     // Code to handle webhook request and pull changes from Github
     //$output = shell_exec('cd /var/www/Ritwell-App && git pull');
     //Log::info("Git pull output: \n" . $output);
-      exec('cd /var/www/Ritwell-App && git pull ', $output, $return_var);
+    exec('cd /var/www/Ritwell-App && git pull ', $output, $return_var);
 
- 
 
-if ($return_var !== 0) {
-    Log::error("Git pull failed with return code: " . $return_var);
-} else {
-    Log::info("Git pull output: \n" . implode("\n", $output));
-    Artisan::call('optimize');
-}
+
+    if ($return_var !== 0) {
+        Log::error("Git pull failed with return code: " . $return_var);
+    } else {
+        Log::info("Git pull output: \n" . implode("\n", $output));
+        Artisan::call('optimize');
+    }
 });
 
 Route::get('/', function () {
