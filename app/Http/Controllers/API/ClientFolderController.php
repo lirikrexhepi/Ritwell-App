a28dd83507eb4f29e54dae11e548ff2963d6887e
Undefined <lirikrexhepiii@gmail.com>
2023-02-22 14:36:46 +0100

    ReturnEmail

--- app/Http/Controllers/API/ClientFolderController.php
+++ app/Http/Controllers/API/ClientFolderController.php
@@ -11,60 +11,52 @@ use App\Models\User;
 
 class ClientFolderController extends Controller
 {
-            public function index()
-            {
-                if(Auth::user()->role=="1"){
-
-                $users = User::select('name')->get();
-                return response()->json($users, 200);
-                }
-                else{
-                    return response()->json(['message' => 'Unauthenticated user']);
-                }
-
-            }
-
-
-
-            public function show($email)
-            {
-                if(Auth::user()->role=="1"){
-                $user = User::where('email', $email)->first();
-                return response()->json($user, 200);
-            }
-            else{
-                return response()->json(['message' => 'Unauthenticated user']);
-            }
-            }
+    public function index()
+    {
+        if (Auth::user()->role == "1") {
+            $users = User::select('name', 'email')->get();
+            return response()->json($users, 200);
+        } else {
+            return response()->json(['message' => 'Unauthenticated user']);
+        }
+    }
 
 
 
 
-            public function clientProperties(Request $request, $email)
-            {
-                $clientProperties = User::where('email', $email)->first();
-                $clientProperties->strongPoints = $request->input('strongPoints');
-                $clientProperties->weakPoints = $request->input('weakPoints');
-                $clientProperties->save();
-
-                return response()->json($clientProperties, 200);
-            }
-
-
-            public function clientPropertiesShow($email)
-            {
-                if(Auth::user()->role=="1"){
-                    $user = User::select('strongPoints', 'weakPoints')
-                                ->where('email', $email)
-                                ->first();
-                    return response()->json($user, 200);
-                }
-                else{
-                    return response()->json(['message' => 'Unauthenticated user']);
-                }
-            }
+    public function show($email)
+    {
+        if (Auth::user()->role == "1") {
+            $user = User::where('email', $email)->first();
+            return response()->json($user, 200);
+        } else {
+            return response()->json(['message' => 'Unauthenticated user']);
+        }
+    }
 
 
 
-            
+
+    public function clientProperties(Request $request, $email)
+    {
+        $clientProperties = User::where('email', $email)->first();
+        $clientProperties->strongPoints = $request->input('strongPoints');
+        $clientProperties->weakPoints = $request->input('weakPoints');
+        $clientProperties->save();
+
+        return response()->json($clientProperties, 200);
+    }
+
+
+    public function clientPropertiesShow($email)
+    {
+        if (Auth::user()->role == "1") {
+            $user = User::select('strongPoints', 'weakPoints')
+                ->where('email', $email)
+                ->first();
+            return response()->json($user, 200);
+        } else {
+            return response()->json(['message' => 'Unauthenticated user']);
+        }
+    }
 }
